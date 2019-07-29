bae06f6ebc213805a0c9f2cbe0a105e0a2f41d47
Calvin Mann <calvinmann@gmail.com>
2019-07-29 14:29:15 +0200

    Changed auth directive to be able to work on the way in or the way out

--- auth/src/index.js
+++ auth/src/index.js
@@ -5,7 +5,11 @@ const ALL = `ALL`;
 const { defaultFieldResolver } = require(`graphql`);
 const { SchemaDirectiveVisitor } = require(`apollo-server`);
 
-function createAuthDirective(requestHasRole, allRole = ALL) {
+function createAuthDirective({
+    authorizeRequest,
+    authorizeResponse,
+    allRole = ALL
+}) {
     return class AuthDirective extends SchemaDirectiveVisitor {
         visitObject(object) {
             const fields = object.getFields();
@@ -32,54 +36,84 @@ function createAuthDirective(requestHasRole, allRole = ALL) {
 
     function fieldAuthWrap(field, next) {
         return async function authorizedResolve(source, args, context, info) {
+            await validateRequest({ source, args, context, info });
+            const response = await next(source, args, context, info);
+            await validateResponse({ source, args, context, info, response });
+            return response;
+        };
+
+        async function validateRequest({ source, args, context, info }) {
+            return validate(({
+                source,
+                args,
+                context,
+                info,
+                type: `request`,
+                validation: authorizeRequest
+            }));
+        }
+
+        function validateResponse({ source, args, context, info, response }) {
+            return validate(({
+                source,
+                args,
+                context,
+                info,
+                response,
+                type: `response`,
+                validation: authorizeResponse
+            }));
+        }
+
+        async function validate({ type, source, args, context, info, validation, response }) {
             const roleData = typeof field.roles === `function` && field.roles();
 
             if (!roleData) {
-                throw new Error(`Unable to authorize request. Role data is missing from field`);
+                throw new Error(`Unable to authorize ${type}. Role data is missing from field`);
             }
             if (!roleData.allow.length) {
-                throw new Error(`Unable to authorize request. No allow role information defined, ` +
-                    `therefore all requests are denied`);
+                throw new Error(`Unable to authorize ${type}. No allow role information defined, ` +
+                    `therefore all are denied`);
             }
 
             if (roleData.deny.includes(allRole)) {
-                throw new Error(`Unable to authorize request. Authorization has been set to deny ${allRole}`);
+                throw new Error(`Unable to authorize ${type}. Authorization has been set to deny ${allRole}`);
             }
 
             const deniedRoles = (await Promise.all(roleData.deny.map(hasRole)))
                 .filter(role => role);
 
             if (deniedRoles.length) {
-                throw new Error(`Unable to authorize request. Authorization has been set to deny ${allRole}, ` +
+                throw new Error(`Unable to authorize ${type}. Authorization has been set to deny ${allRole}, ` +
                     `and the request context has "${deniedRoles}"`);
             }
 
             // Allow pass through if allRole is defined
             if (roleData.allow.includes(allRole)) {
-                return next(source, args, context, info);
+                return;
             }
 
             const allowedRoles = (await Promise.all(roleData.allow.map(hasRole)))
                 .filter(role => role);
 
             if (allowedRoles.length) {
-                // TODO: Stats here?
-                return next(source, args, context, info);
+                return;
             }
 
-            throw new Error(`Unable to authorize request. Request context does not have any ` +
+            throw new Error(`Unable to authorize ${type}. Request context does not have any ` +
                 `of the allowed roles (${roleData.allow.join(`, `)})`);
 
             async function hasRole(role) {
-                const has = await requestHasRole({
+                const has = await validation({
                     role,
                     args,
                     info,
                     source,
-                    context
+                    context,
+                    response
                 });
                 return has && role;
             }
-        };
+        }
     }
 }
